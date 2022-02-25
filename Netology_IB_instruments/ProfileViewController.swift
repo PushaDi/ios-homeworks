6305d3e6ddd50f0fc731a49211f69bca7cbbdc02
Dmitry Khloptsov <dkhloptsov@yandex.ru>
2022-02-25 17:39:41 +0300

    Initial Commit

---
+++ Netology_IB_instruments/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_instruments
+//
+//  Created by Dmitry Khloptsov on 25.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController{
+    
+}
