cd80bd5ef6dc86afe653b9623ec38774d5bc3d5f
Dmitry Khloptsov <dkhloptsov@yandex.ru>
2022-03-14 21:59:40 +0300

    Добавлен стек и две кнопки. Не настроено

--- Netology_IB_instruments/FeedViewController.swift
+++ Netology_IB_instruments/FeedViewController.swift
@@ -13,15 +13,38 @@ class FeedViewController: UIViewController {
         let postTitle: String
     }
 
-    private lazy var postButton: UIButton = {
-        let postButton = UIButton(frame: .zero)
-        return postButton
-    }()
+//    private lazy var postButton: UIButton = {
+//        let postButton = UIButton(frame: .zero)
+//        return postButton
+//    }()
+    
+    private var customStackView = UIStackView()
+    
+    private var firstButton: UIButton = UIButton()
+    private var secondButton: UIButton = UIButton()
+    
+    private func setupStackView() {
+        self.view.addSubview(customStackView)
+        customStackView.axis = .vertical
+        customStackView.spacing = 10
+        
+        customStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        customStackView.addArrangedSubview(self.firstButton)
+        customStackView.addArrangedSubview(self.secondButton)
+        
+        let centerXConstraint = customStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let centerYConstraint = customStackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        
+        NSLayoutConstraint.activate([centerXConstraint, centerYConstraint])
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupView()
-        self.setupButton()
+        self.setupStackView()
+//        self.setupButton()
     }
     
     private func setupView() {
@@ -30,30 +53,30 @@ class FeedViewController: UIViewController {
         
     }
 
-    private func setupButton() {
-        postButton.setTitle("Посмотреть пост", for: .normal)
-        postButton.setTitleColor(.white, for: .normal)
-        postButton.layer.cornerRadius = 12
-        postButton.backgroundColor = .systemBlue
-    
-        postButton.translatesAutoresizingMaskIntoConstraints = false
-        postButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-
-        view.addSubview(postButton)
-        
-        let bottomConstraint = self.postButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10.0)
-        let leadingConstraint = self.postButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10.0)
-        let trailingConstraint = self.postButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10.0)
-        let heightConstraint = self.postButton.heightAnchor.constraint(equalToConstant: 50.0)
-        
-        NSLayoutConstraint.activate([bottomConstraint, leadingConstraint, trailingConstraint, heightConstraint])
-    }
-    @objc private func didTapButton(_ sender: UIButton!) {
-        let postVc = PostViewController()
-        let newPost = self.createPost(postTitle: "Интересная заметка")
-        postVc.text = newPost.postTitle
-        self.navigationController?.pushViewController(postVc, animated: true)
-    }
+//    private func setupButton() {
+//        postButton.setTitle("Посмотреть пост", for: .normal)
+//        postButton.setTitleColor(.white, for: .normal)
+//        postButton.layer.cornerRadius = 12
+//        postButton.backgroundColor = .systemBlue
+//
+//        postButton.translatesAutoresizingMaskIntoConstraints = false
+//        postButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+//
+//        view.addSubview(postButton)
+//
+//        let bottomConstraint = self.postButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -10.0)
+//        let leadingConstraint = self.postButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 10.0)
+//        let trailingConstraint = self.postButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -10.0)
+//        let heightConstraint = self.postButton.heightAnchor.constraint(equalToConstant: 50.0)
+//
+//        NSLayoutConstraint.activate([bottomConstraint, leadingConstraint, trailingConstraint, heightConstraint])
+//    }
+//    @objc private func didTapButton(_ sender: UIButton!) {
+//        let postVc = PostViewController()
+//        let newPost = self.createPost(postTitle: "Интересная заметка")
+//        postVc.text = newPost.postTitle
+//        self.navigationController?.pushViewController(postVc, animated: true)
+//    }
     
     private func createPost(postTitle: String) -> postStruct {
         return postStruct(postTitle: postTitle)
