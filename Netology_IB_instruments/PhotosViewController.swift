31780f97aa86bd0cdbb6c16c852313f132a02ea7
Dmitry Khloptsov <dkhloptsov@yandex.ru>
2022-04-18 15:41:43 +0300

    Добавлена анимация появления фото на весь экран, затемнение заднего фона и появление крестика

--- Netology_IB_instruments/PhotosViewController.swift
+++ Netology_IB_instruments/PhotosViewController.swift
@@ -56,7 +56,6 @@ class PhotosViewController: UIViewController {
         
         NSLayoutConstraint.activate([photosCollectionViewTopConstraint, photosCollectionViewLeadingConstraint, photosCollectionViewTrailingConstraint, photosCollectionViewBottomConstraint])
     }
-    
 }
 
 extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -84,6 +83,82 @@ extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDele
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let side = (UIScreen.main.bounds.width - 8 * 4)/3
         return CGSize(width: side, height: side)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let attributes = collectionView.layoutAttributesForItem(at: indexPath)
+        let cellRect = attributes?.frame
+        let cellFrameInSuperview = collectionView.convert(cellRect ?? CGRect.zero, to: collectionView.superview)
+        
+        let backgroundView: UIView = {
+            let backgroundView = UIView()
+            backgroundView.alpha = 0
+            backgroundView.isHidden = true
+            backgroundView.backgroundColor = .black
+            backgroundView.translatesAutoresizingMaskIntoConstraints = false
+            return backgroundView
+        }()
+       
+        let image = UIImage(named: self.dataSource[indexPath.row])
+        let largeImageView: UIImageView = {
+            let imageView = UIImageView(image: image)
+            imageView.frame = cellFrameInSuperview
+            imageView.isHidden = true
+            imageView.alpha = 0
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        let exitButton: UIImageView = {
+            
+            let exitButton = UIImageView(image: UIImage(systemName: "multiply.square.fill"))
+            exitButton.translatesAutoresizingMaskIntoConstraints = false
+            exitButton.isHidden = true
+            exitButton.isUserInteractionEnabled = true
+            exitButton.alpha = 0
+            
+            return exitButton
+        }()
+        
+        self.view.addSubview(exitButton)
+        self.view.addSubview(backgroundView)
+        self.view.addSubview(largeImageView)
+        
+        let backgroundViewCenterXConstraint = backgroundView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let backgroundViewCenterYConstraint = backgroundView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        let backgroundViewHeightConstraint = backgroundView.heightAnchor.constraint(equalTo: self.view.heightAnchor)
+        let backgroundViewWidthConstraint = backgroundView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+
+        let exitButtonHeightConstraint = exitButton.heightAnchor.constraint(equalToConstant: 50)
+        let exitButtonWidthConstraint = exitButton.widthAnchor.constraint(equalToConstant: 50)
+        let exitButtonTopAnchor = exitButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5)
+        let exitButtonTrailingConstraint = exitButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5)
+        
+        NSLayoutConstraint.activate([exitButtonHeightConstraint, exitButtonWidthConstraint, exitButtonTopAnchor,exitButtonTrailingConstraint, backgroundViewWidthConstraint, backgroundViewHeightConstraint, backgroundViewCenterYConstraint, backgroundViewCenterXConstraint])
+        
+        UIView.animate(withDuration: 0.5) {
+            NSLayoutConstraint.deactivate(largeImageView.constraints)
+            backgroundView.isHidden = false
+            backgroundView.alpha = 0.5
+            self.photosCollectionView.isUserInteractionEnabled = false
+            largeImageView.isHidden = false
+            largeImageView.alpha = 1
+            largeImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            largeImageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+            largeImageView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
+            largeImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
+            largeImageView.contentMode = .scaleAspectFill
+            largeImageView.clipsToBounds = true
+            self.view.layoutIfNeeded()
+        } completion: {_ in
+            UIView.animate(withDuration: 0.3) {
+                exitButton.isHidden = false
+                exitButton.alpha = 1
+            } completion: { _ in
+    
+            }
+        }
     }
     
 }
