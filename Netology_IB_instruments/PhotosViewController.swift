3b256dc1509763bd908429234f2008ca74a12d76
Dmitry Khloptsov <dkhloptsov@yandex.ru>
2022-04-18 17:40:09 +0300

    добавлена функциональность кнопку выхода из полноэкранного отображения фотографии

--- Netology_IB_instruments/PhotosViewController.swift
+++ Netology_IB_instruments/PhotosViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 class PhotosViewController: UIViewController {
     
     private var dataSource: [String] = photoAlbum
+    
+    private let exitButtonTapGestureRecogniser = UITapGestureRecognizer()
+    
+    private lazy var backgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.alpha = 0
+        backgroundView.isHidden = true
+        backgroundView.backgroundColor = .black
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        return backgroundView
+    }()
+    
+    private lazy var largeImageView: UIImageView = {
+        let largeImageView = UIImageView()
+        largeImageView.isHidden = true
+        largeImageView.alpha = 0
+        largeImageView.translatesAutoresizingMaskIntoConstraints = false
+        largeImageView.contentMode = .scaleAspectFill
+        largeImageView.clipsToBounds = true
+        return largeImageView
+    }()
+    
+    private lazy var exitButton: UIImageView = {
+        let exitButton = UIImageView(image: UIImage(systemName: "multiply.square.fill"))
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        exitButton.isHidden = true
+        exitButton.isUserInteractionEnabled = true
+        exitButton.alpha = 0
+        
+        return exitButton
+    }()
 
     private lazy var photosLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
@@ -34,6 +65,7 @@ class PhotosViewController: UIViewController {
         super.viewDidLoad()
         self.configureView()
         self.setupConstraints()
+        self.setupGesture()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -43,9 +75,11 @@ class PhotosViewController: UIViewController {
     
     private func configureView() {
         self.view.addSubview(self.photosCollectionView)
+        self.view.addSubview(backgroundView)
+        self.view.addSubview(exitButton)
+        self.view.addSubview(largeImageView)
         self.title = "Photo Gallery"
         self.navigationController?.navigationBar.isHidden = false
-        
     }
 
     private func setupConstraints() {
@@ -54,7 +88,45 @@ class PhotosViewController: UIViewController {
         let photosCollectionViewTrailingConstraint = self.photosCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         let photosCollectionViewBottomConstraint = self.photosCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         
-        NSLayoutConstraint.activate([photosCollectionViewTopConstraint, photosCollectionViewLeadingConstraint, photosCollectionViewTrailingConstraint, photosCollectionViewBottomConstraint])
+        let backgroundViewCenterXConstraint = self.backgroundView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let backgroundViewCenterYConstraint = self.backgroundView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        let backgroundViewHeightConstraint = self.backgroundView.heightAnchor.constraint(equalTo: self.view.heightAnchor)
+        let backgroundViewWidthConstraint = self.backgroundView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
+
+        let exitButtonHeightConstraint = self.exitButton.heightAnchor.constraint(equalToConstant: 50)
+        let exitButtonWidthConstraint = self.exitButton.widthAnchor.constraint(equalToConstant: 50)
+        let exitButtonTopAnchor = self.exitButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5)
+        let exitButtonTrailingConstraint = self.exitButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5)
+        
+        NSLayoutConstraint.activate([
+            photosCollectionViewTopConstraint, photosCollectionViewLeadingConstraint, photosCollectionViewTrailingConstraint, photosCollectionViewBottomConstraint,
+            exitButtonHeightConstraint, exitButtonWidthConstraint, exitButtonTopAnchor,exitButtonTrailingConstraint,
+            backgroundViewWidthConstraint, backgroundViewHeightConstraint, backgroundViewCenterYConstraint, backgroundViewCenterXConstraint
+                                    ])
+    }
+    
+    private func setupGesture() {
+        self.exitButtonTapGestureRecogniser.addTarget(self, action: #selector(self.handleExitButtonTapGesture(_ :)))
+        self.exitButton.addGestureRecognizer(self.exitButtonTapGestureRecogniser)
+    }
+    
+    @objc private func handleExitButtonTapGesture(_ gestureRecogniser: UITapGestureRecognizer) {
+        guard self.exitButtonTapGestureRecogniser === gestureRecogniser else { return }
+        UIView.animate(withDuration: 0.3) {
+            self.exitButton.isHidden = true
+            self.exitButton.alpha = 0
+            
+            
+        } completion: { _ in
+            UIView.animate(withDuration: 0.5) {
+                self.backgroundView.isHidden = true
+                self.backgroundView.alpha = 0
+                self.photosCollectionView.isUserInteractionEnabled = true
+                self.largeImageView.isHidden = true
+                self.largeImageView.alpha = 0
+            } completion: { _ in
+            }
+        }
     }
 }
 
@@ -90,71 +162,26 @@ extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDele
         let attributes = collectionView.layoutAttributesForItem(at: indexPath)
         let cellRect = attributes?.frame
         let cellFrameInSuperview = collectionView.convert(cellRect ?? CGRect.zero, to: collectionView.superview)
-        
-        let backgroundView: UIView = {
-            let backgroundView = UIView()
-            backgroundView.alpha = 0
-            backgroundView.isHidden = true
-            backgroundView.backgroundColor = .black
-            backgroundView.translatesAutoresizingMaskIntoConstraints = false
-            return backgroundView
-        }()
-       
         let image = UIImage(named: self.dataSource[indexPath.row])
-        let largeImageView: UIImageView = {
-            let imageView = UIImageView(image: image)
-            imageView.frame = cellFrameInSuperview
-            imageView.isHidden = true
-            imageView.alpha = 0
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            return imageView
-        }()
+        self.largeImageView.image = image
         
-        let exitButton: UIImageView = {
-            
-            let exitButton = UIImageView(image: UIImage(systemName: "multiply.square.fill"))
-            exitButton.translatesAutoresizingMaskIntoConstraints = false
-            exitButton.isHidden = true
-            exitButton.isUserInteractionEnabled = true
-            exitButton.alpha = 0
-            
-            return exitButton
-        }()
-        
-        self.view.addSubview(exitButton)
-        self.view.addSubview(backgroundView)
-        self.view.addSubview(largeImageView)
-        
-        let backgroundViewCenterXConstraint = backgroundView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-        let backgroundViewCenterYConstraint = backgroundView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        let backgroundViewHeightConstraint = backgroundView.heightAnchor.constraint(equalTo: self.view.heightAnchor)
-        let backgroundViewWidthConstraint = backgroundView.widthAnchor.constraint(equalTo: self.view.widthAnchor)
-
-        let exitButtonHeightConstraint = exitButton.heightAnchor.constraint(equalToConstant: 50)
-        let exitButtonWidthConstraint = exitButton.widthAnchor.constraint(equalToConstant: 50)
-        let exitButtonTopAnchor = exitButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5)
-        let exitButtonTrailingConstraint = exitButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5)
-        
-        NSLayoutConstraint.activate([exitButtonHeightConstraint, exitButtonWidthConstraint, exitButtonTopAnchor,exitButtonTrailingConstraint, backgroundViewWidthConstraint, backgroundViewHeightConstraint, backgroundViewCenterYConstraint, backgroundViewCenterXConstraint])
-        
+        self.largeImageView.frame = cellFrameInSuperview
         UIView.animate(withDuration: 0.5) {
-            NSLayoutConstraint.deactivate(largeImageView.constraints)
-            backgroundView.isHidden = false
-            backgroundView.alpha = 0.5
+            NSLayoutConstraint.deactivate(self.largeImageView.constraints)
+            self.backgroundView.isHidden = false
+            self.backgroundView.alpha = 0.5
             self.photosCollectionView.isUserInteractionEnabled = false
-            largeImageView.isHidden = false
-            largeImageView.alpha = 1
-            largeImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-            largeImageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-            largeImageView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
-            largeImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
-            largeImageView.contentMode = .scaleAspectFill
-            largeImageView.clipsToBounds = true
+            self.largeImageView.isHidden = false
+            self.largeImageView.alpha = 1
+            self.largeImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.largeImageView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+            self.largeImageView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
+            self.largeImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width).isActive = true
             self.view.layoutIfNeeded()
         } completion: {_ in
             UIView.animate(withDuration: 0.3) {
-                exitButton.isHidden = false
-                exitButton.alpha = 1
+                self.exitButton.isHidden = false
+                self.exitButton.alpha = 1
             } completion: { _ in
     
             }
